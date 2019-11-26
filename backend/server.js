911d988dae9060a3443cdaf929afdfb1fa877ece
Pearl McPhee <Pearl.McPhee@siriusxm.com>
2019-11-26 12:08:15 -0500

    created chat based worry tree exercise

--- backend/server.js
+++ backend/server.js
@@ -12,20 +12,73 @@ const logger = require('morgan');
 // const router = app.Router();
 const manager = new NlpManager({ languages: ['en'] });
 // Adds the utterances and intents for the NLP
+// Hello Goodbye
 manager.addDocument('en', 'goodbye for now', 'greetings.bye');
 manager.addDocument('en', 'bye bye take care', 'greetings.bye');
 manager.addDocument('en', 'okay see you later', 'greetings.bye');
+manager.addDocument('en', 'bye bitch', 'greetings.bye');
 manager.addDocument('en', 'bye for now', 'greetings.bye');
 manager.addDocument('en', 'i must go', 'greetings.bye');
+manager.addDocument('en', 'tah tah', 'greetings.bye');
 manager.addDocument('en', 'hello', 'greetings.hello');
 manager.addDocument('en', 'hi', 'greetings.hello');
 manager.addDocument('en', 'howdy', 'greetings.hello');
+manager.addDocument('en', "what's up", 'greetings.hello');
+manager.addDocument('en', "sup", 'greetings.hello');
+manager.addDocument('en', "hey boo", 'greetings.hello');
 
 // Train also the NLG
 manager.addAnswer('en', 'greetings.bye', 'Till next time');
-manager.addAnswer('en', 'greetings.bye', 'see you soon!');
+manager.addAnswer('en', 'greetings.bye', 'Byeeeee!');
+manager.addAnswer('en', 'greetings.bye', 'Bye!');
+manager.addAnswer('en', 'greetings.bye', 'Alright then!');
+manager.addAnswer('en', 'greetings.bye', 'See you soon!');
 manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+manager.addAnswer('en', 'greetings.hello', 'Hi!');
+manager.addAnswer('en', 'greetings.hello', "What's up");
+
+// Worry Tree
+manager.addDocument('en', "start worry", 'worrytree.started');
+manager.addDocument('en', "worry start", 'worrytree.started');
+manager.addDocument('en', "again", 'worrytree.started');
+manager.addDocument('en', "trigger worry", 'worrytree.started');
+manager.addDocument('en', "I have another worry", 'worrytree.started');
+manager.addDocument('en', "start worrytree", 'worrytree.started');
+manager.addDocument('en', "start worry tree", 'worrytree.started');
+manager.addDocument('en', "worry tree start", 'worrytree.started');
+manager.addDocument('en', "my worry is", 'worrytree.userWorry');
+manager.addDocument('en', "I'm worried about", 'worrytree.userWorry');
+manager.addDocument('en', "This is my worry", 'worrytree.userWorry');
+manager.addDocument('en', "tackle worry no", 'worrytree.noTackleWorry');
+manager.addDocument('en', "tackle worry nope", 'worrytree.noTackleWorry');
+manager.addDocument('en', "tackle worry I can't", 'worrytree.noTackleWorry');
+manager.addDocument('en', "tackle worry yes", 'worrytree.yesTackleWorry');
+manager.addDocument('en', "tackle worry yup", 'worrytree.yesTackleWorry');
+manager.addDocument('en', "tackle worry I can", 'worrytree.yesTackleWorry');
+manager.addDocument('en', "tackle worry yes I can", 'worrytree.yesTackleWorry');
+manager.addDocument('en', "timing tackle worry now", 'worrytree.nowTackleWorry');
+manager.addDocument('en', "timing tackle worry right now", 'worrytree.nowTackleWorry');
+manager.addDocument('en', "timing tackle worry yes", 'worrytree.nowTackleWorry');
+manager.addDocument('en', "timing tackle worry yup", 'worrytree.nowTackleWorry');
+manager.addDocument('en', "timing tackle worry no", 'worrytree.laterTackleWorry');
+manager.addDocument('en', "timing tackle worry nope", 'worrytree.laterTackleWorry');
+manager.addDocument('en', "timing tackle worry later", 'worrytree.laterTackleWorry');
+
+
+manager.addAnswer('en', 'worrytree.started', "What are you worried about?");
+manager.addAnswer('en', 'worrytree.started', "Tell me what you're worried about?");
+manager.addAnswer('en', 'worrytree.started', "What worry is on your mind right now?");
+manager.addAnswer('en', 'worrytree.userWorry', "Is this worry something you can do something about?");
+manager.addAnswer('en', 'worrytree.userWorry', "Is it possible for you to do something about it?");
+manager.addAnswer('en', 'worrytree.userWorry', "Is this something that you can solve?");
+manager.addAnswer('en', 'worrytree.userWorry', "Is this worry within your control?");
+manager.addAnswer('en', 'worrytree.noTackleWorry', "Let this worry go and change your focus!");
+manager.addAnswer('en', 'worrytree.noTackleWorry', "Let it go. Let it go. Let it go. And then focus on something else!");
+manager.addAnswer('en', 'worrytree.noTackleWorry', "Good news! You can let it go and then focus on something else!");
+manager.addAnswer('en', 'worrytree.yesTackleWorry', "Is this something you can do now or later?");
+manager.addAnswer('en', 'worrytree.yesTackleWorry', "Great! Is this something you can do now or later?");
+manager.addAnswer('en', 'worrytree.nowTackleWorry', "Do it! And then change your focus.");
+manager.addAnswer('en', 'worrytree.laterTackleWorry', "Schedule it! And then change your focus.");
 
 async function train() {
     // Train the model
--- client/src/components/WorryTree.js
+++ client/src/components/WorryTree.js
@@ -1,36 +1,18 @@
 import React from 'react'
 import ChatList from './ChatList'
+import axios from 'axios'
+
 import './ChatBox.css'
 export default class WorryTree extends React.Component  {
     constructor() {
         super()
-        this.questions = [
-            {
-                question: "What are you worrying about?",
-                answers: 'user-input'
-            },
-            {
-                question: "Is this worry something I can do something about?",
-                answers: ['Yes', 'No']
-            },
-            {
-                responded: 'No',
-                followUp: ["Let worry go", "Change focus of attention"]
-            },
-            {
-                responded: 'Yes',
-                question: "Is this something you can do now or later?",
-                answers: ['Now', 'Later']
-            },
-            {
-                responded: 'Now',
-                followUp: ['Do it!', 'Let worry go', 'Change focus of attention']
-            },
-            {
-                responded: 'Later',
-                followUp: ['Schedule it', 'Let worry go', 'Change focus of attention']
-            }
+        this.userResponses = [
+            {prompt: 'start worry'},
+            {prompt: 'my worry is'},
+            {prompt: 'tackle worry', add: true},
+            {prompt: 'timing tackle worry', add: true}
         ]
+       
         this.state = {
             nextQuestion: 1,
             currentQuestion: 0,
@@ -47,35 +29,73 @@ export default class WorryTree extends React.Component  {
                 }
             ],
             newChat: {},
-            chats: [{text: this.questions[0].question, user: {
-                id: 0,
-                profilePic: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hookedonhallmark.com%2Fassets%2Fimages%2F2015%2F1795qx9007.jpg&f=1&nofb=1',
-                isBot: true
-            }}]
+            chats: []
         }
     }
 
-    handleClick = (e) => {
-        const nextQuestion = this.state.nextQuestion
-        const currentQuestion = this.state.currentQuestion
-
-        // Check if current question has answers
-        const currentQHasAnswers = currentQuestion.answers
-
-        // if current question has answers, check the answer
-        let newNextQuestion
-        if (currentQHasAnswers) {
-            const answer = e.target.innerText
-            newNextQuestion = this.questions.find(question => question.responded === answer)
+    addChat = (e, chat) => {
+        e.preventDefault && e.preventDefault()
+       
+        const text = chat || this.refs.newChatText.value;
+        if (!chat) {
+            const newChat = {
+                text,
+                user: this.state.users[1]
+            }
+            const newChatList = this.state.chats.concat(newChat)
+            this.setState({
+                chats: newChatList
+            })
+    
+            this.refs.newChatText.value = ""
+            setTimeout(() => {
+                // send the right prompt to the bot
+                const currentResponse = this.userResponses[this.state.currentQuestion] || ''
+                const userResponse = currentResponse.add ? `${currentResponse.prompt}  ${text}` : currentResponse.prompt 
+                this.addBotReply(userResponse)
+                const currentQ = this.state.currentQuestion
+                this.setState({
+                    currentQuestion: currentQ === 3 ? 1 : this.state.currentQuestion + 1
+                })
+            }, 500)
+            return
         }
+        
+        setTimeout(() => {
+            this.addBotReply(chat)
+        }, 500)
+    }
+
+    addBotReply = (chat) => {
+        axios.post('/newUserMsg', {
+            text: chat || this.state.chats[this.state.chats.length - 1].text,
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "*",
+            },
+        })
+        .then(res => {
+            const newChat = {
+                text: res.data.answer || "I'm sorry, I don't understand.",
+                user: this.state.users[0]
+            }
+            const newChatList = this.state.chats.concat(newChat)
+            this.setState({
+                chats: newChatList,
+            })
+        })
+        .catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+        this.addChat({}, this.userResponses[0].prompt)
         this.setState({
-            nextQuestion: newNextQuestion || nextQuestion + 1,
             currentQuestion: this.state.currentQuestion + 1
         })
     }
 
-    render() {
 
+    render() {
 
         return (
             <article className="WorryTree">
@@ -84,7 +104,7 @@ export default class WorryTree extends React.Component  {
                 <ChatList messages={this.state.chats} />
                 <article className="ChatBox--input-container">
                     <form onSubmit={this.addChat}>
-                        <input ref="newChatText" onChange={this.handleChange}  placeholder="say something..." />
+                        <input ref="newChatText" placeholder="say something..." />
                         <button type="submit" >send</button>
                     </form>
                 </article>
